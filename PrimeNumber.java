c830296f35eca7d5ec1c943bbfe2ee3ad425005f
AshaFazis <107307864+AshaFazis@users.noreply.github.com>
2022-06-11 20:50:09 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+package week1.day1.assignments;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int Input= 13;
+		boolean flag = false;
+		for(int i=2; i< Input/2; i++)
+		  
+	   {   
+			
+		   if(Input%2==0)
+		   {
+			   System.out.println("Prime number");
+		   }
+	       else
+	       {
+	    	   System.out.println("Not a Prime number");
+	       }
+		
+	   //}
+
+	}
+
+}
